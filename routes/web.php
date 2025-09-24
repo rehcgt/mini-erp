7f05b980c6e69154e7d29dbfa8386b2612227fdf
Raul Hernandez <rehcgt@outlook.com>
2025-09-24 16:57:48 -0600

    adding prefix to match the subfolder used in EC2

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+// Agregar el prefijo 'mini-erp' a todas las rutas definidas en este grupo ya que el proyecto esta hospedado en una subcarpeta
+// Si el proyecto va a estar en la raiz del dominio, eliminar el prefijo
+Route::prefix('mini-erp')->group(function () {
 
-Route::view('/', 'welcome');
+    Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+    Route::view('dashboard', 'dashboard')
+        ->middleware(['auth', 'verified'])
+        ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+    Route::view('profile', 'profile')
+        ->middleware(['auth'])
+        ->name('profile');
 
-require __DIR__.'/auth.php';
+    require __DIR__.'/auth.php'; // si auth.php define rutas, también estarán bajo /mini-erp
+});
