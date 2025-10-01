03b1308391838b36cf0eb23232b176ca6074e0be
Raul Hernandez <rehcgt@outlook.com>
2025-10-01 16:17:45 -0600

    fixing issue with livewire assets

--- config/livewire.php
+++ config/livewire.php
@@ -1,10 +1,29 @@
 <?php
 
 return [
-    // ...otras configuraciones por defecto de Livewire...
-
+    'manifest_path' => null,
+    'app_url' => null,
     'asset_url' => env('LIVEWIRE_ASSET_URL', '/mini-erp'),
-
-    // Puedes agregar aquí otras configuraciones si lo necesitas
+    'middleware' => [
+        'web',
+    ],
+    'alias' => 'livewire',
+    'class_namespace' => 'App\\Livewire',
+    'view_path' => resource_path('views/livewire'),
+    'layout' => 'components.layouts.app',
+    'render_on_redirect' => false,
+    'temporary_file_upload' => [
+        'disk' => null,
+        'rules' => null,
+        'directory' => null,
+        'middleware' => null,
+        'preview_mimes' => [
+            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4', 'mov', 'avi', 'wmv', 'mp3', 'm4a', 'webm', 'ogg', 'oga', 'flac', 'aac', 'opus', 'jpg', 'jpeg', 'webp',
+        ],
+        'max_upload_time' => 5,
+    ],
+    'inject_assets' => true,
+    'inject_morph_markers' => true,
+    'legacy_model_binding' => false,
+    'features' => [],
 ];
-
