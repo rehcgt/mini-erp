e871e8d6648a29036d7cbb068b434d829555763d
Raul Hernandez <rehcgt@outlook.com>
2025-10-01 16:10:45 -0600

    fixing issue with livewire assets

---
+++ config/livewire.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    // ...otras configuraciones por defecto de Livewire...
+
+    'asset_url' => env('LIVEWIRE_ASSET_URL', '/mini-erp'),
+
+    // Puedes agregar aquí otras configuraciones si lo necesitas
+];
+
