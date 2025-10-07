8488c9072df8a45b14c6e0d51262216ededea082
Raul Hernandez <rehcgt@outlook.com>
2025-10-07 17:46:15 -0600

    fixing error in view

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -57,7 +57,7 @@ new class extends Component
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div x-data="{{ json_encode(['name' => auth()->user()->name]) }}"
+                            <div x-data="{{ json_encode(['name' => auth()->user()?->name ?? '']) }}"
                                  x-text="name"
                                  x-on:profile-updated.window="name = $event.detail.name">
                             </div>
